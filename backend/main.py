786d12a3a9ac80d727a448cd46c7f56ef332f738
JakePIXL <jakewjevans@gmail.com>
2023-03-22 00:24:29 -0400

    moved broken import

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
-import uvicorn
 from application import get_application
 
 app = get_application()
 
 
 def start():
+    import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True, root_path="")
 
 
